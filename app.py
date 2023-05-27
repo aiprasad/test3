1d95a21148af380b0bb183189188334db8e199b4
aiprasad <49321587+aiprasad@users.noreply.github.com>
2023-05-27 07:06:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#import all the required libraries
 import torch
 from flask import Flask, render_template, request
 from instruct_pipeline import InstructionTextGenerationPipeline
@@ -5,9 +6,11 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 app = Flask(__name__)
 
+#initialize tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-3b", padding_side="left")
 model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-3b", device_map="auto", torch_dtype=torch.bfloat16)
 
+#create a text generation pipeline
 generate_text = InstructionTextGenerationPipeline(model=model, tokenizer=tokenizer)
 
 @app.route('/')
@@ -16,9 +19,11 @@ def index():
 
 @app.route('/generate')
 def generate():
+    #prompt from the request arguments
     prompt = request.args.get('prompt')
     if not prompt:
         return render_template('index.html')
+    #generates text based on the prompt
     generated_text = generate_text(prompt)
     return render_template('index.html', prompt=prompt, generated_text=generated_text)
 
